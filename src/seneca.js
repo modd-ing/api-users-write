d6f90839c79077e426d022ada444631a8b6e8dfa
Ante Sepic <antesepic@gmail.com>
2016-07-01 00:31:52 +0200

    Add seneca transport connection to api-authorize

--- src/seneca.js
+++ src/seneca.js
@@ -27,6 +27,11 @@ seneca.ready( function( err ) {
       pin: 'role:api,path:users,type:read',
       type: 'amqp',
       url: 'amqp://rabbitmq-api'
+    })
+    .client({
+      pin: 'role:api,path:authorize',
+      type: 'amqp',
+      url: 'amqp://rabbitmq-api'
     });
 
 });
